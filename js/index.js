ce40d135995905b3dbc8a72b1ab0862cbf3f6208
Thomas Lacheheb <thomas.lacheheb@hotmail.fr>
2021-02-03 21:28:46 +0100

    Ajout des évènements des clics sur les boutons de couleurs + les boutons start & reset

--- js/index.js
+++ js/index.js
@@ -1,3 +1,15 @@
 const $buttons = document.querySelectorAll("button");
+const $buttonContainer = document.querySelector("#button_container");
+const $divButtons = $buttonContainer.querySelectorAll("div");
 
-console.log($buttons);
+$buttons.forEach((button) => {
+    button.addEventListener("click", ({ target: { innerHTML = "" } }) => {
+        console.log(innerHTML);
+    });
+});
+
+$divButtons.forEach((button) => {
+    button.addEventListener("click", ({ target: { id = "" } }) => {
+        console.log(id);
+    });
+});
